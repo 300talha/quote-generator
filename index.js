e19a328cb362abc48cbf901195bad79ddcda9fc2
300talha <120650613+300talha@users.noreply.github.com>
2023-05-16 15:43:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
-    // Move JavaScript code to a separate JS file
+document.addEventListener('DOMContentLoaded', function() {
+  const generateBtn = document.getElementById('generateBtn');
+  const quoteElement = document.getElementById('quote');
 
-    document.addEventListener("DOMContentLoaded", function () {
-      const quoteTextElement = document.getElementById("quoteText");
-      const quoteAuthorElement = document.getElementById("quoteAuthor");
-      const newQuoteBtn = document.getElementById("newQuoteBtn");
-
-      // Fetch a new quote from the API
-      function fetchQuote() {
-        fetch("https://quotes-api-self.vercel.app/quote")
-          .then(response => response.json())
-          .then(data => {
-            const { quote, author } = data;
-            quoteTextElement.textContent = quote;
-            quoteAuthorElement.textContent = `- ${author}`;
-          })
-          .catch(error => {
-            console.error("Error fetching quote:", error);
-          });
-      }
-
-      // Generate a new quote on button click
-      newQuoteBtn.addEventListener("click", fetchQuote);
-
-      // Fetch an initial quote on page load
-      fetchQuote();
-    });
+  generateBtn.addEventListener('click', function() {
+    fetch('https://quotes-api-self.vercel.app/quote')
+      .then(response => response.json())
+      .then(data => {
+        quoteElement.textContent = data.quote;
+      })
+      .catch(error => {
+        console.log(error);
+        quoteElement.textContent = 'Failed to fetch a quote.';
+      });
+  });
+});
