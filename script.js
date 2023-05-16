79053970d448a083a20c4712b20cf752e45a6743
300talha <120650613+300talha@users.noreply.github.com>
2023-05-16 15:48:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
   const generateBtn = document.getElementById('generateBtn');
   const quoteElement = document.getElementById('quote');
+  const authorElement = document.getElementById('author');
 
   generateBtn.addEventListener('click', function() {
     fetch('https://quotes-api-self.vercel.app/quote')
       .then(response => response.json())
       .then(data => {
         quoteElement.textContent = data.quote;
+        authorElement.textContent = '- ' + data.author;
       })
       .catch(error => {
         console.log(error);
         quoteElement.textContent = 'Failed to fetch a quote.';
+        authorElement.textContent = '';
       });
   });
 });
